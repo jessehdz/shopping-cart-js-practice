c07cffebaa243cf4ca5fda85336ebb4b153f4006
Jesse Hernandez <hdzj_@outlook.com>
2025-09-08 20:43:27 -0700

    docs(script.js): replace starter code comments with personalized function explanations

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,4 +1,4 @@
-/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+// Array of products
 const products = [
   {
     name: "Cherry",
@@ -28,14 +28,10 @@ function findProductById(productId) {
   return products.find((product) => product.productId === productId);
 }
 
-/* Declare an empty array named cart to hold the items in the cart */
+// Empty array to hold the items in the cart for the user
 const cart = [];
 
-/* Create a function named addProductToCart that takes in the product productId as an argument
-  - addProductToCart should get the correct product based on the productId
-  - addProductToCart should then increase the product's quantity
-  - if the product is not already in the cart, add it to the cart
-*/
+// Adds products to cart using "Add to Cart" button
 function addProductToCart(productId) {
   const product = findProductById(productId);
   if (!product) return;
@@ -45,21 +41,14 @@ function addProductToCart(productId) {
   }
 }
 
-/* Create a function named increaseQuantity that takes in the productId as an argument
-  - increaseQuantity should get the correct product based on the productId
-  - increaseQuantity should then increase the product's quantity
-*/
+// Increases product quantity using "+" button or "Add to Cart"
 function increaseQuantity(productId) {
   const product = findProductById(productId);
   if (!product) return;
   product.quantity++;
 }
 
-/* Create a function named decreaseQuantity that takes in the productId as an argument
-  - decreaseQuantity should get the correct product based on the productId
-  - decreaseQuantity should decrease the quantity of the product
-  - if the function decreases the quantity to 0, the product is removed from the cart
-*/
+// Decreases product quantity using "-" button and removes product if quantity is 0
 function decreaseQuantity(productId) {
   const product = findProductById(productId);
   if (!product) return;
@@ -70,11 +59,7 @@ function decreaseQuantity(productId) {
   }
 }
 
-/* Create a function named removeProductFromCart that takes in the productId as an argument
-  - removeProductFromCart should get the correct product based on the productId
-  - removeProductFromCart should update the product quantity to 0
-  - removeProductFromCart should remove the product from the cart
-*/
+// Removes product from cart and sets quantity to 0 using "Remove" button
 function removeProductFromCart(productId) {
   const product = findProductById(productId);
   if (!product) return;
@@ -82,11 +67,7 @@ function removeProductFromCart(productId) {
   cart.splice(cart.indexOf(product), 1);
 }
 
-/* Create a function named cartTotal that has no parameters
-  - cartTotal should iterate through the cart to get the total cost of all products
-  - cartTotal should return the total cost of the products in the cart
-  Hint: price and quantity can be used to determine total cost
-*/
+// Returns total cost of products in cart
 function cartTotal() {
   let total = 0;
   for (let i = 0; i < cart.length; i++) {
@@ -95,17 +76,12 @@ function cartTotal() {
   return total;
 }
 
-/* Create a function called emptyCart that empties the products from the cart */
+// Function that empties the products from the cart
 function emptyCart() {
   cart.length = 0;
 }
 
-/* Create a function named pay that takes in an amount as an argument
-  - amount is the money paid by customer
-  - pay will return a negative number if there is a remaining balance
-  - pay will return a positive number if money should be returned to customer
-  Hint: cartTotal function gives us cost of all the products in the cart  
-*/
+// Accepts amount paid by customer and returns change or remaining balance
 let totalPaid = 0;
 function pay(amount) {
   totalPaid += amount;
@@ -118,14 +94,6 @@ function pay(amount) {
   return difference;
 }
 
-/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
-
-/* The following is for running unit tests. 
-  To fully complete this project, it is expected that all tests pass.
-  Run the following command in terminal to run tests
-  npm run test
-*/
-
 module.exports = {
   products,
   cart,
@@ -134,8 +102,6 @@ module.exports = {
   decreaseQuantity,
   removeProductFromCart,
   cartTotal,
-  // pay,
+  pay,
   emptyCart,
-  /* Uncomment the following line if completing the currency converter bonus */
-  // currency
 };
